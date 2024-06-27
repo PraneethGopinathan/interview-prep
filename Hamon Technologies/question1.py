de695868aa524c6176ec4d0c34e4cf6dc72ae61a
praneeth <praneethgopinathan@gmail.com>
2024-06-27 22:19:39 +0530

    refactored

---
+++ Hamon Technologies/question1.py
@@ -0,0 +1,29 @@
+
+def f(s):
+    r = {}
+    for i in s:
+        if i in r:
+            r[i] += 1
+        else:
+            r[i] = 1
+    return r
+
+def test_empty_string():
+    assert f("") == {}
+
+def test_single_character():
+    assert f("a") == {'a': 1}
+
+def test_repeated_characters():
+    assert f("aa") == {'a': 2}
+
+def test_multiple_characters():
+    assert f("ab") == {'a': 1, 'b': 1}
+
+def test_mixed_characters():
+    assert f("aabbcc") == {'a': 2, 'b': 2, 'c': 2}
+
+def test_special_characters():
+    assert f("a!a!b@b") == {'a': 2, '!': 2, 'b': 2, '@': 1}
+
+
